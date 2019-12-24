60d547fce34ed0acb9871def53760f0dffa3eb88
zhengqh <1150173792@qq.com>
2019-12-24 17:36:02 +0800

    命令行commit

--- src/main/java/kafkastudy/kafka/demo/KafkaProducerCase.java
+++ src/main/java/kafkastudy/kafka/demo/KafkaProducerCase.java
@@ -33,13 +33,13 @@ public class KafkaProducerCase {
 	
 	public static void main(String[] args) {
 		 Properties props = new Properties();
-		 props.put("bootstrap.servers", ipAndPort);
+		 /*props.put("bootstrap.servers", ipAndPort);
          props.put("acks", "all");
          props.put("retries", 0);
          props.put("batch.size", 16384);
          props.put("buffer.memory", 33554432);
          props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");*/
 
          producer = new KafkaProducer<>(props);
          
